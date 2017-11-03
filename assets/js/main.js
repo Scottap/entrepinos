2de9e986f37cd07192cb011e4935f24d4c94d29b
Flavio Ruiz <scottap302@gmail.com>
2017-11-03 14:19:58 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$("#rustic").click(function() {
+  $(".modal").addClass("is-active");  
+});
+
+$(".modal-close").click(function() {
+   $(".modal").removeClass("is-active");
+});
+
+$(document).ready(function(){
+  $('.slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+
+var mymap = L.map('mapid').setView([-34.9164446, -54.8776211], 15);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZnJ1aXoiLCJhIjoiY2o5aW52OHE4M29iODJxczRnZHo4a3hkeSJ9.KjNrmGGXWJZHbSkCLUVC6w'
+}).addTo(mymap);
+
+var marker = L.marker([-34.9164446, -54.8776211]).addTo(mymap);
+
+$('.covervid-video').coverVid(1920, 1080);
